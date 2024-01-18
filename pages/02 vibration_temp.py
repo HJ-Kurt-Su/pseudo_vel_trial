0563bd163a571139b79d8a39a1730c2489795dfc
HJ-Kurt-Su <phononobserver@gmail.com>
2024-01-18 13:47:42 +0800

    update_vibration_fft-Y240118
    - add generate white noise & make fft

--- pages/02 vibration_temp.py
+++ pages/02 vibration_temp.py
@@ -2,6 +2,8 @@ import math
 import numpy as np
 import plotly.express as px
 import streamlit as st
+import pandas as pd
+import endaq
 # from matplotlib import pyplot as plt
 
 
@@ -13,32 +15,62 @@ def main():
     st.markdown("               ")
     st.markdown("               ")
 
-    SIGMA = 4.0
-    ALPHA = 0.2
-    Lc = 25
+    """
+    產生白噪音時間訊號。
 
-    L = 2000.0
-    N = 1000
-    NN = int(N/2)
+    Args:
+    n_samples: 訊號長度。
+    sample_rate: 取樣率。
+    stddev: 標準差。
 
-    dz = L/N
+    Returns:
+    包含白噪音時間訊號的 Pandas dataframe。
+    """
 
-    i = np.concatenate([np.linspace(0,N/2,NN+1), np.linspace(-NN+1,-1,NN-1)])
+    # 產生白噪音資料。
+    stddev = 1
+    n_samples = 10000
+    sample_rate = 200
 
-    f = i/(N*dz)
-    k = 2*np.pi*f
-    PSD = lambda x: (2*3*SIGMA**2*Lc)/(1+(x*Lc)**2)**(0.5+ALPHA)
+    y = np.random.normal(0, stddev, n_samples)
 
-    magnitude = N*np.sqrt(PSD(k))
+    # 建立新的 Pandas dataframe。
+    df_wn = pd.DataFrame({
+    "time": np.arange(0, n_samples / sample_rate, 1 / sample_rate),
+    "y": y,
+    })
 
-    phase = 2*np.pi*np.random.randn(N)
-    FFT = magnitude * np.exp(1j*phase)
-    
-    IFFT = np.fft.ifft(FFT)
+    df_wn
+    aa = df_wn.describe()
+    aa
+    fig_wn = px.line(df_wn, x="time", y="y",
+                    # color_discrete_sequence=color_sequence, template=template, 
+                # log_x=True, log_y=True,
+                # range_x=[1, 1000], range_y=[1, 1000]
+                labels={
+                    "value": "value_d"
+                },
+                )
 
-    magnitude
-    FFT
-    IFFT
+    st.plotly_chart(fig_wn, use_container_width=True)
+    df_wn = df_wn.set_index(df_wn.columns[0])
+
+    # df_fft = endaq.calc.fft.fft(df_wn, output="magnitude")
+    df_fft = endaq.calc.fft.aggregate_fft(df_wn, bin_width=5)
+    # df_fft_al = endaq.calc.fft.fft(df_wn, output="complex")
+    df_fft
+    # df_fft_al
+    df_fft = df_fft.reset_index()
+
+    fig_fft = px.line(df_fft, x='frequency (Hz)', y="y",
+                # color_discrete_sequence=color_sequence, template=template, 
+            # log_x=True, log_y=True,
+            # range_x=[1, 1000], range_y=[1, 1000]
+            labels={
+                "value": "mag"
+            },
+            )
+    st.plotly_chart(fig_fft, use_container_width=True)
 
     # fig = plt.figure()
 
